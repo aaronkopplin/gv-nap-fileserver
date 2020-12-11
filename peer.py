e45d3582d96afcccb5f82ab55dd421f97689ae50
aaron <kopplina@mail.gvsu.edu>
2020-12-11 15:57:08 -0500

    adding client connecting to server

--- peer.py
+++ peer.py
@@ -1,12 +1,21 @@
 import socket
 import globals
 
-#
-# class Peer:
-#     def __init__(self):
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as peer:
-    peer.connect((globals.HOST, globals.PORT))
-    while True:
-        message = input("> ")
-        peer.sendall(message.encode())
+while True:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as peer:
+        try:
+            peer.connect((globals.HOST, globals.PORT))
+            message = input("> ")
+            if message == "exit": break
+            peer.sendall(message.encode())
+        except:
+            choice = input("the host is not online. Try again? (y/n)\n")
+            if choice == "y":
+                continue
+            elif choice == "n":
+                break
+            else:
+                print("error: unreadable input.")
+                break
+
